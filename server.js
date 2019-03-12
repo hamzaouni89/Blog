e37f54cc9be7ac255a77456a670b7fc62bbf24c0
hamzainfo89 <hamzaouni@gmail.com>
2019-03-12 12:23:24 +0100

    add+delete commentaire

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express()
 var db = require('./database/db')
 var users = require('./api/ApiUser')
 var article = require('./api/ApiArticle')
-//var commentaire = require('./api/ApiCom')
+var commentaire = require('./api/ApiCom')
 
 var cors = require('cors');
 var bodyParser = require('body-parser')
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
 app.use('/users', users)
 app.use('/article', article)
-//app.use('/commentaire', commentaire)
+app.use('/commentaire', commentaire)
 
 app.listen(3000, (err => {
 if (err) throw err;
